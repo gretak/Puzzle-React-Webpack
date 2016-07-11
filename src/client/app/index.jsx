96c71fd036d7ab463721cd680ebfb7e05378487d
Greta Kavaliauskaite <greta.kavaliauskaite@cohaesus.co.uk>
2016-07-11 12:14:07 +0100

    added sass to the build (need tidying up)

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
+import HeaderComponent from './HeaderComponent.jsx';
+
+require('./style.scss');
+
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <p> Hello React test!</p>
+        <p> Hello React test test!</p>
         <AwesomeComponent />
+        <HeaderComponent />
       </div>
     );
   }
